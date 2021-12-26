5cc1a8ef43e0434bfd47069e3fc485000493eb37
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2021-12-26 14:54:44 -0600

    added a bit of code to start over again.

---
+++ animal_husbandry.py
@@ -0,0 +1,11 @@
+""" Original code and notes posted to repository by
+Carl Mäsak
+masak
+and can still be found at:
+https://github.com/masak/farm
+"""
+
+
+
+if __name__ == '__main__':
+    print('hello world')
