0cd696696aee2e19f2d22e5f3fbd8561eb2310f2
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2021-12-26 16:51:16 -0600

    refactored intro print statement to a custom funciton.

--- animal_husbandry.py
+++ animal_husbandry.py
@@ -5,11 +5,9 @@ and can still be found at:
 https://github.com/masak/farm
 """
 
+# imports
+from print_blocks import print_intro
 
 
 if __name__ == '__main__':
-    print('hello, and welcome to the Animal Husbandry Game.')
-    print('This game was originally made by Karol Borsuk.')
-    print('There are several articles about this game.')
-    print('My personal thought:')
-    print('\tThis is an important reminder about why we program.')
+    print_intro()
---
+++ print_blocks.py
@@ -0,0 +1,6 @@
+def print_intro():
+    print('hello, and welcome to the Animal Husbandry Game.')
+    print('This game was originally made by Karol Borsuk.')
+    print('There are several articles about this game.')
+    print('My personal thought:')
+    print('\tThis is an important reminder about why we program.')
