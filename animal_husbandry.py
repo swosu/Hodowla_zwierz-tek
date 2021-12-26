8e688544ca5fd0fc1c02fb02991973a151728345
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2021-12-26 16:46:38 -0600

    added a welcome message.

--- animal_husbandry.py
+++ animal_husbandry.py
@@ -8,4 +8,8 @@ https://github.com/masak/farm
 
 
 if __name__ == '__main__':
-    print('hello world')
+    print('hello, and welcome to the Animal Husbandry Game.')
+    print('This game was originally made by Karol Borsuk.')
+    print('There are several articles about this game.')
+    print('My personal thought:')
+    print('\tThis is an important reminder about why we program.')
